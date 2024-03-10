6df998ba66d8fa9e01096fb527ddf040a9c8a54c
unknown <benladen1917@inbox.ru>
2024-03-10 22:29:50 +0300

    feat: swagger reading api.yml

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
-  const config = new DocumentBuilder()
-    .setTitle('Home Library Service')
-    .setDescription('Home music library service')
-    .setVersion('1.0.0')
-    .addTag('Rest service')
-    .build();
   const app = await NestFactory.create(AppModule);
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = await readFile('./doc/api.yaml', { encoding: 'utf-8' });
+  SwaggerModule.setup('doc', app, parse(document));
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
